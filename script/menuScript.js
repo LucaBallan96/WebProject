1d8a113b7bc4e689caefdadf92ea4a3de859efe1
Giovanni <giovannicalore96@gmail.com>
2017-12-13 08:48:22 +0100

    Script e Brochure

---
+++ script/menuScript.js
@@ -0,0 +1,5 @@
+function display_navbar(cm) {
+    document.getElementById("navbar").style.display="block";
+    document.getElementById("close_menu").style.display="block";
+    cm.style.display="none";
+}
