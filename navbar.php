9c616016d1239835143e4f3cf06d0124479085ee
LucaBallan96 <34268184+LucaBallan96@users.noreply.github.com>
2017-12-18 15:40:35 +0100

    Finito menu mobile

--- navbar.php
+++ navbar.php
@@ -8,23 +8,23 @@
         <div id="navbar">
             <a id="home_link" href="index.html"><img id="logo" src="images/logo.png"></a>
             <div class="dropdown">
-                <button class="drop_button">Azienda</button>
-                <div class="drop_content">
+                <button id="menu_bt1" class="drop_button" onclick="display_content(this)">Azienda</button>
+                <div id="content_menu_bt1" class="drop_content">
                     <a href="#"><pre>Storia&#9&#9&gt</pre></a>
                     <a href="#"><pre>Sedi&#9&#9&#9&gt</pre></a>
                     <a href="#"><pre>Persone&#9&#9&gt</pre></a>
                 </div>
             </div>
             <div class="dropdown">
-                <button class="drop_button">Progetti</button>
-                <div class="drop_content">
+                <button id="menu_bt2" class="drop_button" onclick="display_content(this)">Progetti</button>
+                <div id="content_menu_bt2" class="drop_content">
                     <a href="progetti.html"><pre>In corso&#9&#9&gt</pre></a>
                     <a href="#"><pre>Terminati&#9&#9&gt</pre></a>
                 </div>
             </div>
             <div class="dropdown">
-                <button class="drop_button">Iniziative</button>
-                <div class="drop_content">
+                <button id="menu_bt3" class="drop_button" onclick="display_content(this)">Iniziative</button>
+                <div id="content_menu_bt3" class="drop_content">
                     <a href="#"><pre>Sponsor&#9&#9&#9&gt</pre></a>
                     <a href="#"><pre>Stampa&#9&#9&#9&gt</pre></a>
                     <a href="#"><pre>Lavora con Noi&#9&gt</pre></a>
--- script/menuScript.js
+++ script/menuScript.js
@@ -1,5 +1,5 @@
 window.addEventListener("resize", function() {
-    var x=screen.width.valueOf();
+    var x=screen.width;
     if(window.matchMedia("screen and (min-width:720px)").matches) {
         document.getElementById("navbar").style.left="0%";
         document.getElementById("compact_menu").style.display="none";
@@ -17,4 +17,14 @@ function display_navbar(cm) {
 function close_navbar(cl) {
     document.getElementById("navbar").style.left="-100%";
     document.getElementById("compact_menu").style.display="block";
+}
+function display_content(dropbtn) {
+    if(dropbtn.id.endsWith("_open")) {
+        dropbtn.id=dropbtn.id.replace("_open","");
+        document.getElementById("content_"+dropbtn.id).style.display="none";
+    }
+    else {
+        document.getElementById("content_"+dropbtn.id).style.display="block";
+        dropbtn.id += "_open";
+    }
 }
