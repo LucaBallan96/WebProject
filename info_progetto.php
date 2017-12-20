02175b5b33990469bbdacd3be119eab38fadeab8
LucaBallan96 <34268184+LucaBallan96@users.noreply.github.com>
2017-12-20 14:42:53 +0100

    Tolto database condiviso

--- info_progetto.php
+++ info_progetto.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html>
-	<head>
+    <head>
 		<title>Costruzioni Bordignon S.r.l.</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="UTF-8">
 		<meta name="description" content="Costruzioni Bordignon S.r.l., con oltre tre generazioni di storia alle spalle, opera nel settore edile residenziale, industriale, pubblico, nei restauri e nelle infrastrutture mirando ed ottenendo sempre grande apprezzamento dai suoi Clienti, grazie alla sua esperienza, dedizione e continua innovazione">
 		<meta name="keywords" content="Edilizia, settore edile, Treviso, Costruzioni Bordignon">
 		<meta name="author" content="Luca Ballan, Giovanni Calore">
-		<link rel="stylesheet" media="screen and (min-width:1024px)" href="style/progetti/progetti_desktop.css">
-		<link rel="stylesheet" media="screen and (max-width:1024px) and (min-width:720px)" href="style/progetti/progetti_tablet.css">
-		<link rel="stylesheet" media="screen and (max-width:720px)" href="style/progetti/progetti_mobile.css">
-		<link rel="stylesheet" media="print" href="style/progetti/progetti_print.css">
+		<link rel="stylesheet" media="screen and (min-width:720px)" href="style/info_progetto/info_progetto_desktop.css">
+		<link rel="stylesheet" media="screen and (max-width:720px)" href="style/info_progetto/info_progetto_mobile.css">
+		<link rel="stylesheet" media="print" href="style/info_progetto/info_progetto_print.css">
 	</head>
 	<body>
-        <!--SCRIPT-->
+		<!--SCRIPT-->
 		<script type="text/javascript" src="script/menuScript.js"></script>
 
 		<!-- NAVIGATION -->
@@ -52,77 +51,5 @@
             <a href="admin.php" id="admin">Area Privata</a>
             <button id="close" onclick="close_navbar(this)">X</button>
         </div>
-
-		<!-- HEADER -->
-		<h1 id="header1">PROGETTI IN CORSO</h1>
-
-		<!-- PROGETTI -->
-
-		<?php include 'queries/get_progetti.php';?>
-
-		<!--
-		<a href="info_progetto.php" class="project">
-			<img class="pr_image" src="images/progetto1.jpg"/>
-			<div class="pr_title"><div>Titolo progetto</div></div>
-		</a>
-		<div class="project">
-			<img class="pr_image" src="images/progetto2.jpg"/>
-		</div>
-		<div class="project">
-			<img class="pr_image" src="images/progetto3.jpg"/>
-		</div>
-		<div class="project">
-			<img class="pr_image" src="images/progetto4.jpg"/>
-		</div>
-		<div class="project">
-			<img class="pr_image" src="images/progetto5.jpg"/>
-		</div>
-		<div class="project">
-			<img class="pr_image" src="images/progetto6.jpg"/>
-		</div>
-		<div class="project">
-			<img class="pr_image" src="images/progetto7.jpg"/>
-		</div>
-		-->
-
-		<!-- HEADER -->
-		<h1 id="header2">PROGETTI TERMINATI</h1>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<p>Ciao</p>
-	</body>
+    </body>
 </html>
---
+++ queries/get_progetti.php
@@ -0,0 +1,26 @@
+<?php
+    $servername = "localhost:3306";
+	$username = "root";
+	$password = "tecweb2017";
+    // Create connection
+	$conn = new mysqli($servername, $username, $password);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT * FROM progetti.in_corso";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+	// output data of each row
+    	while($row = $result->fetch_assoc()) {
+	    	echo "<a href='info_progetto.php' class='project'>
+					<img class='pr_image' src='images/".$row['image']."'/>
+					<div class='pr_title'><div>".$row['title']."</div></div>
+		        </a>";
+		}
+		$result->free();
+	} else {
+		echo "<p>Nessun progetto disponibile</p>";
+	}
+	$conn->close();
+?>
