87cf6cec73748568ef70b2a84417921998911bbd
LucaBallan96 <34268184+LucaBallan96@users.noreply.github.com>
2017-12-27 18:55:18 +0100

    Pagina login
    Per Admin e utenti

--- admin.php
+++ admin.php
@@ -12,44 +12,6 @@
 		<link rel="stylesheet" media="print" href="style/admin/admin_print.css">
 	</head>
 	<body>
-		<!--SCRIPT-->
-		<script type="text/javascript" src="script/menuScript.js"></script>
-
-		<!-- NAVIGATION -->
-		<button id="compact_menu" onclick="display_navbar(this)">
-            <div class="cm_line"></div>
-            <div class="cm_line"></div>
-            <div class="cm_line"></div>
-        </button>
-
-        <div id="navbar">
-            <a id="home_link" href="index.html"><img id="logo" src="images/logo.png"></a>
-            <div class="dropdown">
-                <button id="menu_bt1" class="drop_button" onclick="display_content(this)">Azienda</button>
-                <div id="content_menu_bt1" class="drop_content">
-                    <a href="azienda.php"><pre>Storia&#9&#9&gt</pre></a>
-                    <a href="azienda.php"><pre>Sedi&#9&#9&#9&gt</pre></a>
-                    <a href="azienda.php"><pre>Persone&#9&#9&gt</pre></a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button id="menu_bt2" class="drop_button" onclick="display_content(this)">Progetti</button>
-                <div id="content_menu_bt2" class="drop_content">
-                    <a href="progetti.php"><pre>In corso&#9&#9&gt</pre></a>
-                    <a href="progetti.php#header2"><pre>Terminati&#9&#9&gt</pre></a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button id="menu_bt3" class="drop_button" onclick="display_content(this)">Iniziative</button>
-                <div id="content_menu_bt3" class="drop_content">
-                    <a href="iniziative.php"><pre>Sponsor&#9&#9&#9&gt</pre></a>
-                    <a href="iniziative.php"><pre>Stampa&#9&#9&#9&gt</pre></a>
-                    <a href="iniziative.php"><pre>Lavora con Noi&#9&gt</pre></a>
-                </div>	
-            </div>
-            <a href="#div_container_contatti" id="contacts">Contatti</a>
-            <a href="admin.php" id="admin">Area Privata</a>
-            <button id="close" onclick="close_navbar(this)">X</button>
-        </div>
+        
     </body>
 </html>
