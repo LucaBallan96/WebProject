ae2f19ef03bf003ccf7c8c3fbe8df566b36e40fe
LucaBallan96 <34268184+LucaBallan96@users.noreply.github.com>
2018-01-02 12:28:55 +0100

    Modifiche admin

--- admin.php
+++ admin.php
@@ -7,20 +7,129 @@
 		<meta name="description" content="Costruzioni Bordignon S.r.l., con oltre tre generazioni di storia alle spalle, opera nel settore edile residenziale, industriale, pubblico, nei restauri e nelle infrastrutture mirando ed ottenendo sempre grande apprezzamento dai suoi Clienti, grazie alla sua esperienza, dedizione e continua innovazione">
 		<meta name="keywords" content="Edilizia, settore edile, Treviso, Costruzioni Bordignon">
 		<meta name="author" content="Luca Ballan, Giovanni Calore">
-		<link rel="stylesheet" media="screen and (min-width:721px)" href="style/admin/admin_desktop.css">
+		<link rel="stylesheet" media="screen and (min-width:1025px)" href="style/admin/admin_desktop.css">
+		<link rel="stylesheet" media="screen and (max-width:1024px) and (min-width:721px)" href="style/admin/admin_tablet.css">
 		<link rel="stylesheet" media="screen and (max-width:720px)" href="style/admin/admin_mobile.css">
 		<link rel="stylesheet" media="print" href="style/admin/admin_print.css">
 	</head>
 	<body>
 		<div id="nav">
-			<a href="index.html" id="nav_home" class="nav_link">Home</a>
-			<a href="#impiegati" id="nav_people" class="nav_link">Impiegati</a>
-			<a href="#progetti" id="nav_projects" class="nav_link">Progetti</a>
-			<a href="#articoli" id="nav_articles" class="nav_link">Articoli</a>
-			<a href="#utenti" id="nav_users" class="nav_link">Utenti</a>
-			<a href="#lavoro" id="nav_work" class="nav_link">Lavoro</a>
+			<a title="Home" href="index.html" id="nav_home" class="nav_link"><div>Home</div></a>
+			<a title="Impiegati" href="#impiegati" id="nav_people" class="nav_link"><div>Impiegati</div></a>
+			<a title="Progetti" href="#progetti" id="nav_projects" class="nav_link"><div>Progetti</div></a>
+			<a title="Articoli" href="#articoli" id="nav_articles" class="nav_link"><div>Articoli</div></a>
+			<a title="Utenti" href="#utenti" id="nav_users" class="nav_link"><div>Utenti</div></a>
+			<a title="Lavoro" href="#lavoro" id="nav_work" class="nav_link"><div>Lavoro</div></a>
 		</div>
 		<h1 id="impiegati" class="header">Impiegati e dirigenti d'azienda</h1>
+		<div id="container_imp">
+			<div id="left_container">
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato1.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"><div>Mario Rossi</div></div>
+						<div class="imp_birth"><div>01-01-1970</div></div>
+						<div class="imp_role"><div>Presidente</div></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check1" class="check_control"/>
+				<label class="btn" for="check1">
+					<div class="magic">
+						<div class="imp_more"><div>Età: 47 anni</div></div>
+						<div class="imp_more"><div>Settore: direttivo</div></div>
+						<div class="imp_more"><div>Impiegato dell'azienda dal 2002</div></div>		
+					</div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato2.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"><div>Annamaria Bianchi</div></div>
+						<div class="imp_birth"><div>31-12-1985</div></div>
+						<div class="imp_role"><div>Vicepresidente</div></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check2" class="check_control"/>
+				<label class="btn" for="check2">
+					<div class="magic"></div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato3.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"></div>
+						<div class="imp_role"></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check3" class="check_control"/>
+				<label class="btn" for="check3">
+					<div class="magic"></div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato4.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"></div>
+						<div class="imp_role"></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check4" class="check_control"/>
+				<label class="btn" for="check4">
+					<div class="magic"></div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+			</div>
+			<div id="right_container">
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato5.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"></div>
+						<div class="imp_role"></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check5" class="check_control"/>
+				<label class="btn" for="check5">
+					<div class="magic"></div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato6.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"></div>
+						<div class="imp_role"></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check6" class="check_control"/>
+				<label class="btn" for="check6">
+					<div class="magic"></div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato7.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"></div>
+						<div class="imp_role"></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check7" class="check_control"/>
+				<label class="btn" for="check7">
+					<div class="magic"></div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+				<div class="div_impiegato">
+					<div class="imp_image" style="background-image:url('../../images/impiegato8.jpg')"></div>
+					<div class="imp_info">
+						<div class="imp_name"></div>
+						<div class="imp_role"></div>
+					</div>
+				</div>
+				<input type="checkbox" id="check8" class="check_control"/>
+				<label class="btn" for="check8">
+					<div class="magic"></div>
+					<div class="more_info"><div>+</div></div>
+				</label>
+			</div>
+		</div>
 		<h1 id="progetti" class="header">Progetti in cui si impegna l'azienda</h1>
 		<h1 id="articoli" class="header">Articoli e news</h1>
 		<h1 id="utenti" class="header">Utenti del sito</h1>
