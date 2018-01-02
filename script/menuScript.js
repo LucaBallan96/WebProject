d5a93b567a4d4cc756c4bc7252c6618cd48d5547
LucaBallan96 <34268184+LucaBallan96@users.noreply.github.com>
2018-01-02 13:20:31 +0100

    modifiche

--- script/menuScript.js
+++ script/menuScript.js
@@ -1,6 +1,5 @@
 window.addEventListener("resize", function() {
-    var x=screen.width;
-    if(window.matchMedia("screen and (min-width:721px)").matches) {
+    if(window.matchMedia("screen and (min-width:1025px)").matches) {
         document.getElementById("navbar").style.left="0%";
         document.getElementById("compact_menu").style.display="none";
     }
