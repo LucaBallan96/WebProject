a61dd0b097b2ce726ed9ca95890e5a0038e17192
Giovanni <giovannicalore96@gmail.com>
2018-01-04 12:31:36 +0100

    azienda.php

--- azienda.php
+++ azienda.php
@@ -27,9 +27,9 @@
             <div class="dropdown">
                 <button id="menu_bt1" class="drop_button" onclick="display_content(this)">Azienda</button>
                 <div id="content_menu_bt1" class="drop_content">
-                    <a href="azienda.php"><pre>Storia&#9&#9&gt</pre></a>
-                    <a href="azienda.php"><pre>Sedi&#9&#9&#9&gt</pre></a>
-                    <a href="azienda.php"><pre>Persone&#9&#9&gt</pre></a>
+                    <a href="azienda.php#storia"><pre>Storia&#9&#9&gt</pre></a>
+                    <a href="azienda.php#sedi"><pre>Sedi&#9&#9&#9&gt</pre></a>
+                    <a href="azienda.php#persone"><pre>Persone&#9&#9&gt</pre></a>
                 </div>
             </div>
             <div class="dropdown">
@@ -51,5 +51,125 @@
             <a href="login.php?compilato=0" id="admin">Area Privata</a>
             <button id="close" onclick="close_navbar(this)">X</button>
         </div>
+
+
+         <!--TITLE-->
+        
+    
+    <div id="title">La Nostra Azienda</div>
+
+
+    <!--STORIA-->
+
+    <div class="container">
+        <div id="storia"class="subtitle">Storia</div>
+        <div class="divisor"></div>
+        <div id="text_storia" class="text_general">
+            <div class="text">La <a href="index.html">Costruzioni Bordignon S.r.l.</a> nasce nel 1998, sulle orme della tradizione iniziata nei primi anni del 1900 dal nonno degli attuali titolari, e proseguita poi per mano del padre degli stessi, fino all’avvicendamento generazionale. 
+        </br></br>Nel corso di tre generazioni l’azienda si è continuamente distinta per la professionalità dimostrata, puntando sempre sulla qualità delle opere fornite, nel rispetto delle condizioni e dei tempi stabiliti. Grazie all’impegno di attrezzature e macchinari altamente specializzati, e al supporto di uno staff tecnico specializzato, è in grado di offrire un servizio che le permette di primeggiare nel settore. La <a href="https://it.wikipedia.org/wiki/Norme_della_serie_ISO_9000" target="_blanck">certificazione di qualità ISO</a>, l'<a href="https://it.wikipedia.org/wiki/Societ%C3%A0_organismi_di_attestazione" class="link_generico" target="_black">attestazione SOA</a> e
+			l'<a href="https://it.wikipedia.org/wiki/OHSAS_18001" class="link_generico" target="_blanck">attestazione OHSAS</a> sono la conferma dell’impegno profuso al raggiungimento di livelli costruttivi all’avanguardia, che consentono di realizzare opere che vanno dai grandi complessi residenziali ed industriali, ai piccoli lavori civili che tuttavia contribuiscono ad accrescere l’esperienza, fondamentale per un continuo miglioramento professionale. 
+        </br></br>La continua qualificazione e l’incremento dell’organico permettono inoltre di rendere l’azienda sempre più flessibile ed attenta alle esigenze, ai particolari e agli standard richiesti dalla committenza al giorno d’oggi. 
+        </br></br>
+        Puntualità, professionalità e qualità sono i segni di riconoscimento che la Costruzioni Bordignon S.r.l. si è guadagnata nel corso degli anni distinguendosi sempre, grazie soprattutto ai continui investimenti in tecnologia, attrezzatura, ricerca e sviluppo e al supporto di uno staff tecnico interno specializzato e in continuo aggiornamento. 
+    </br></br>Con tali competenze l’azienda si immette nel mercato con grande autorità, conquistando un posto di rilievo nel settore. Per soddisfare tutte le esigenze, l'azienda ha selezionato i settori di attività onde garantire al cliente qualunque servizio in modo autonomo e senza vincoli da terzi, ottenendo così il controllo diretto ed assoluto della qualità fornita ed una maggiore essibilità d’intervento. 
+        Sulla base degli insegnamenti e del know now di Costruzioni Bordignon S.r.l., nasce nel 2013 la B.G.P. GENERAL BAU S.r.l. per garantire la stessa competenza, professionalità e vicinanza anche nell’esecuzione di opere site nell’area Altoatesina. 
+    </br></br>Conferma dell’alto standard qualitativo e professionale con cui le aziende si presentano nel mercato sono le certi cazioni del sistema di qualità rispondente alle norme UNI EN ISO 9001, le certificazioni SOA e la certi cazione OHSAS 18001, implementazione del Sistema di Gestione per la Salute e Sicurezza sul Lavoro ottentuta nel 2012. 
+        A Gennaio 2017 si è ottenuta un’ulteriore conferma della qualità e della professionalità di Costruzioni Bordignon S.r.l., dimostrata dall’inserimento della società nell’elenco delle imprese con Rating di Legalità, per cui ha ottenuto il punteggio di: ★++ .
+        </div>
+    </div>
+
+        <div id="container_img_storia">
+            <div class="container_img"><img class="img_storia"src="images/1.png"/></div>
+            <div class="container_img"><img class="img_storia"src="images/2.png"/></div>
+            <div class="container_img"><img class="img_storia"src="images/3.png"/></div>
+        </div>
+    </div>
+
+
+    <!--SEDI-->
+
+    <div class="container">
+        <div id="sedi"class="subtitle">Sedi</div>
+        <div class="divisor"></div>
+        <div id="introduction_text" class="text_general">
+            <div class="text">Le aree Costruzioni Bordignon S.r.l. e B.G.P. General Bau S.r.l. sono così suddivise:</br></br>
+        
+            
+            <ul>
+                <li> <span>Sede Legale Costruzioni Bordignon S.r.l.</span> - con superficie di mq 700,00 ove è ben 
+                    chiara la distinzione tra i settori <i>amministrativo, tecnico, direzionale ed immobiliare con show-room</i>. 
+                    Tale organizzazione permette di lavorare in armonia, in maniera 
+                    indipendente e autonoma, ma nel contempo consente una stretta collaborazione costruttiva tra le parti.</br></br>
+                </li>
+                <li> <span>Sede Legale General Bau S.r.l.</span> - situata nel centro di Bressanone, sede nevralgica per la gestione dei lavori nell'area altoatesina.</br></br>
+                </li>
+                <li> <span>Laboratori Prove</span> - attrezzati per poter testare materiali quali inerti, cementi, componenti in cotto e finiture, nonchè provini di 
+                    calcestruzzo e acciai per cemento armato per il continuo monitoraggio dei processi produttivi interni ed esterni. </br></br>
+                </li>
+                <li> <span>Officine-Depositi</span> - indispensabili per poter garantire l'efficenza e la reperibilità dell'attrezzatura, in particolare meccanica ed elettronica. </br></br>
+                </li>
+                <li> <span>Magazzini</span> - con superficie di mq 2.550,00 dedicati alla custodia e allo stoccaggio degli automezzi, delle macchine operatrici e dell'attrezzatura varia nelle diverse 
+                    sedi aziendali, soprattutto nei periodi di trasferimento dei cantieri. </br></br>
+                </li>
+                <li> <span>Aree Scoperte</span> - con superficie di mq 12.100,00 adibite a parcheggio, lavorazione ferro,
+                    stoccaggio materiali, scarico e carico merci per una maggiore efficienza operativa.</br></br>
+                </li>
+            </ul>
+        
+        
+        
+            Oltre a queste strutture, fanno parte integrante dell' azienda altre sedi dislocate nel
+            territorio con funzione di supporto logistico.
+            </div>
+        </div>
+
+        <div id="container_img_sedi">
+                <iframe id ="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44522.22438841073!2d12.051779077956512!3d45.778424079105896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47793ae8658b48a7%3A0x94926af6ccab5215!2sCostruzioni+Bordignon+Srl!5e0!3m2!1sit!2sit!4v1514980483579"  frameborder="0" style="border:0" allowfullscreen></iframe>
+                <img id="img_persone"src="images/sede.png"title="sede Costruzioni Bordignon S.r.l."/>
+            </div>
+
+    </div>
+    
+
+
+    <!--PERSONE-->
+
+
+    <div class="container">
+        <div id="persone"class="subtitle">Persone</div>
+        <div class="divisor"></div>
+        <div id="text_persone" class="text_general">
+            <div class="text">
+            Costruzioni Bordignon S.r.l. è costituita sotto forma di società a responsabilità limitata il cui capitale sociale è diviso al 50% tra i due soci.
+            La società è governata attraverso un consiglio di amministrazione i cui componenti attualmente in carica sono:</br></br>
+            Geom. <span>Bordignon Paolo</span> - Presidente</br>
+            Geom. <span>Bordignon Gianantonio</span> - Vicepresidente</br>
+        </br></br>
+            È presente inoltre un collegio sindacale composto da:</br></br>
+            Dott. BASSO Ermanno – Presidente</br>
+            Dott. BAGGIO Roberto – Sindaco effettivo</br>
+            Dott. INNOCENTE Mauro – Sindaco effettivo</br>
+            Dott.ssa CARRARO Cristina – Sindaco supplente</br>
+            Dott.ssa MISCIATTELLI Alessandra – Sindaco Supplente</br>
+        </br></br>
+        <a href="index.html">Costruzioni Bordignon S.r.l.</a> ha inoltre le seguenti partecipazioni societarie:</br></br>
+            Bordignon Service Srl con sede a Volpago del Montello (TV) – Quota capitale partecipato 100%</br>
+            Immobiliare Amalia Srl con sede a Volpago del Montello (TV)- Quota capitale partecipato 100%</br>
+            B.G.P. General Bau Srl con sede a Bressanone (BZ) – Quota capitale partecipato 100%</br></br>
+        </div>
+    </div>
+
+    <div id="container_img_persone"><img id="organigramma"src="images/organigramma.png"/></div>
+    </div>
+    
+
+<!--IMPIEGATI-->
+<?php 
+    
+    include "DBConnection.php";
+    $conn=new DBConnection();    
+    $conn->get_impiegati();
+
+?>
     </body>
 </html>
