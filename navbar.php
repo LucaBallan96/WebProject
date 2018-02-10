5b6ff29604ea48adb9665e2d69c9b20e107d4ec4
Giovanni <giovannicalore96@gmail.com>
2018-02-10 18:19:13 +0100

    logo menu

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@ echo"<input id='compact_menu' onclick='changeTitle()' type='checkbox'/>
     <div class='cm_line'></div>
     <div class='cm_line'></div>
     </div>
-    <div id='cont_img'><img src='images/logo.png'/></div>
+    <div id='cont_img'><img id ='imgl' src='images/logo.png'/></div>
 </label>
 
 <div id='navbar'>
